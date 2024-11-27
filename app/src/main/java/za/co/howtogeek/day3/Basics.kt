0bfc491bf880b01a9604c6129ecdc19b4c366857
DaHub49 <dyls.martin.49@gmail.com>
2024-11-27 15:04:35 +0200

    27/11/24: Added makeCoffee function.

---
+++ app/src/main/java/za/co/howtogeek/day3/Basics.kt
@@ -0,0 +1,13 @@
+package za.co.howtogeek.day3
+
+fun main(){
+    makeCoffee()
+}
+
+fun makeCoffee(){
+    println("Order coffee beans from Pause")
+    println("Grind beans in Rancillio")
+    println("Heat water to 79C")
+    println("Invert Aeropres")
+    println("Steep coffee and press Aeropress")
+}
