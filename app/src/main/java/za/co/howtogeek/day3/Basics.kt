9ce9613c251b97c874fef2c3408cdbcee75e1ad8
DaHub49 <dyls.martin.49@gmail.com>
2024-11-28 06:51:19 +0200

    28/11/24 @06:51: Quiz 9

--- app/src/main/java/za/co/howtogeek/day3/Basics.kt
+++ app/src/main/java/za/co/howtogeek/day3/Basics.kt
@@ -1,14 +1,16 @@
 package za.co.howtogeek.day3
 
 fun main(){
-    makeCoffee(1)
-    makeCoffee(2)
-    makeCoffee(3)
+    makeCoffee(1, "Amy")
+    makeCoffee(2, "Jaco")
+    makeCoffee(3, "Fred")
+    makeCoffee(0, "Marco")
 }
 
-fun makeCoffee(sugarCount: Int){
-    if (sugarCount == 1){
-        println("Coffee with $sugarCount spoon of sugar.")
-    } else
-        println("Coffee with $sugarCount spoons of sugar.")
+fun makeCoffee(sugarCount: Int, name: String){
+    when(sugarCount){
+        0 -> println("Coffee with no spoons of sugar for $name.")
+        1 -> println("Coffee with $sugarCount spoon of sugar for $name.")
+        else -> println("Coffee with $sugarCount spoons of sugar for $name.")
+    }
 }
