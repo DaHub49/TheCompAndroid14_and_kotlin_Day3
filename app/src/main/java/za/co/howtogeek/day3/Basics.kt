7d5f81134f6f848f454c53ac60e4207803c5956f
DaHub49 <dyls.martin.49@gmail.com>
2024-11-28 12:09:16 +0200

    28/11/24 @12:08: Video 52

--- app/src/main/java/za/co/howtogeek/day3/Basics.kt
+++ app/src/main/java/za/co/howtogeek/day3/Basics.kt
@@ -1,11 +1,13 @@
 package za.co.howtogeek.day3
 
 fun main(){
-    println("Please enter 2 numbers you'd like to add, pressing <Enter> after each.")
+    println("Please enter 2 numbers you'd like to add, and then devide, pressing <Enter> after each.")
     val num1 = readln().toInt()
     val num2 = readln().toInt()
 
     println("The addition of $num1 and $num2 = ${add(num1, num2)}")
+
+    println("The division of $num1 by $num2 = ${divide(num1.toDouble(), num2.toDouble())}")
 
     do {
         println("Who is this coffee for?")
@@ -20,6 +22,11 @@ fun main(){
 
 fun add(num1: Int, num2: Int): Int {
     var result = num1 + num2
+    return result
+}
+
+fun divide(num1: Double, num2: Double): Double {
+    var result: Double = num1/num2
     return result
 }
 
