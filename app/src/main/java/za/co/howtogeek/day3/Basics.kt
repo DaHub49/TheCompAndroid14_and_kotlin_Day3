a349416c63a9eadaa25721f4cd2b92c79f95d90e
DaHub49 <dyls.martin.49@gmail.com>
2024-11-28 08:23:14 +0200

    28/11/24 @08:22: Quiz 10

--- app/src/main/java/za/co/howtogeek/day3/Basics.kt
+++ app/src/main/java/za/co/howtogeek/day3/Basics.kt
@@ -1,15 +1,31 @@
 package za.co.howtogeek.day3
 
 fun main(){
-    makeCoffee(1, "Amy")
-    makeCoffee(2, "Jaco")
-    makeCoffee(3, "Fred")
-    makeCoffee(0, "Marco")
+    println("Please enter 2 numbers you'd like to add, pressing <Enter> after each.")
+    val num1 = readln().toInt()
+    val num2 = readln().toInt()
+
+    println("The addition of $num1 and $num2 = ${add(num1, num2)}")
+
+    do {
+        println("Who is this coffee for?")
+        var name: String = readln()
+        if (!name.equals("")) {
+            println("How many sugars do you want with your coffee?")
+            var sugarCount = readln().toInt()
+            makeCoffee(name, sugarCount)
+        }
+    }while (!name.equals(""))
 }
 
-fun makeCoffee(sugarCount: Int, name: String){
+fun add(num1: Int, num2: Int): Int {
+    var result = num1 + num2
+    return result
+}
+
+fun makeCoffee(name: String, sugarCount: Int){
     when(sugarCount){
-        0 -> println("Coffee with no spoons of sugar for $name.")
+        0 -> println("Coffee with no sugar for $name.")
         1 -> println("Coffee with $sugarCount spoon of sugar for $name.")
         else -> println("Coffee with $sugarCount spoons of sugar for $name.")
     }
