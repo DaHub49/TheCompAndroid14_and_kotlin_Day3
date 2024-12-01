af7d182c1f1333d4e148cc7956ecd67bd22cb9d3
DaHub49 <dyls.martin.49@gmail.com>
2024-12-01 12:40:20 +0200

    01/12/24 @12:39 Day 3- Video 58: Implemented functions to clean up the code and commented out earlier exercises.

--- app/src/main/java/za/co/howtogeek/day3/Basics.kt
+++ app/src/main/java/za/co/howtogeek/day3/Basics.kt
@@ -8,6 +8,7 @@ data class CoffeeDetails(
 )
 
 fun main(){
+    /*
     // Creating myBook object:
     var myBook = Book()
     var customBook = Book("Highway to Hades", "Dylan Martin", 2014)
@@ -16,34 +17,37 @@ fun main(){
     println("Book itle: ${customBook.title}, book author: ${customBook.author}, year published: ${customBook.yearPublished}")
     //println("{myBook.cuatomToString()}: ${myBook.customToString()}")
     //println("{customBook.customToString()}: $customBook.customToString()")
+     */
 
-
+    /*
     // Creating a Dog object:
     var bella = Dog("Bella", "Miniature Schnauzer", 9)
     println("${bella.name} is a ${bella.breed} and is ${bella.age} years old.\n")
     println("A year has passed.")
     bella.age++
     println("\n${bella.name} is now ${bella.age} years old.")
+     */
 
+    /*
     println("Please enter 2 numbers you'd like to add, and then devide, pressing <Enter> after each.")
     val num1 = readln().toInt()
     val num2 = readln().toInt()
 
     println("The addition of $num1 and $num2 = ${add(num1, num2)}")
-
     println("The division of $num1 by $num2 = ${divide(num1.toDouble(), num2.toDouble())}")
+     */
 
-    val coffeeForDyls = CoffeeDetails(sugarCount = 0, name = "Dylan", "standard", 0)
+    // custom CoffeeDetails object:
+    //val coffeeForDyls = CoffeeDetails(sugarCount = 0, name = "Dylan", "standard", 0)
 
-    do {
-        println("Who is this coffee for?")
-        var name: String = readln()
-        if (!name.equals("")) {
-            println("How many sugars do you want with your coffee?")
-            var sugarCount = readln().toInt()
-            makeCoffee(coffeeForDyls)
-        }
-    }while (!name.equals(""))
+    var name = orderCoffee()
+    if (!name.equals("")) {
+        val customCoffee =
+            CoffeeDetails(sugarCount(), name = name, size = coffeeSize(), amountOfCream())
+        makeCoffee(customCoffee)
+    }
+    else
+        println("Thank you, $name. Come again!")
 }
 
 fun add(num1: Int, num2: Int): Int {
@@ -56,11 +60,38 @@ fun divide(num1: Double, num2: Double): Double {
     return result
 }
 
+fun orderCoffee(): String{
+    println("Who is this coffee for?")
+    val name: String = readln()
+    return name
+}
+
+fun coffeeSize(): String {
+    var coffeeSize = "250"
+    println("What size coffee would you like?")
+    coffeeSize = readln()
+    return coffeeSize
+}
+
+fun sugarCount(): Int {
+    var sugarCountVar: Int = 0
+    println("How many sugars do you want with your coffee?")
+    sugarCountVar = readln().toInt()
+    return sugarCountVar
+} // sugarCount
+
+fun amountOfCream(): Int {
+    var creamCountVar: Int = 0
+    println("How much cream do you want with your coffee in ml?")
+    creamCountVar = readln().toInt()
+    return creamCountVar
+}
+
 // can use data class asparameters for functions:
 fun makeCoffee(coffeeDetails: CoffeeDetails){
     when(coffeeDetails.sugarCount){
-        0 -> println("Coffee with no sugar for ${coffeeDetails.name} and ${coffeeDetails.amountOfCream}ml of cream.")
-        1 -> println("Coffee with ${coffeeDetails.sugarCount} spoon of sugar for ${coffeeDetails.name} and ${coffeeDetails.amountOfCream}ml of cream.")
-        else -> println("Coffee with ${coffeeDetails.sugarCount} spoons of sugar for ${coffeeDetails.name} and ${coffeeDetails.amountOfCream}ml of cream.")
+        0 -> println("${coffeeDetails.size} coffee with no sugar for ${coffeeDetails.name} and ${coffeeDetails.amountOfCream}ml of cream.")
+        1 -> println("${coffeeDetails.size} coffee with ${coffeeDetails.sugarCount} spoon of sugar for ${coffeeDetails.name} and ${coffeeDetails.amountOfCream}ml of cream.")
+        else -> println("${coffeeDetails.size} coffee with ${coffeeDetails.sugarCount} spoons of sugar for ${coffeeDetails.name} and ${coffeeDetails.amountOfCream}ml of cream.")
     }
 }
